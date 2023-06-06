da70c45461e87d2bd4c67c735a7719b20d68895f
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-06-06 23:55:35 +0200

    finished

---
+++ script.js
@@ -0,0 +1,27 @@
+
+/*  1)Display the document title in the console*/
+
+// console.log(document.title)
+
+
+/* 2)  Change the document title to Modifying the DOM  */
+
+// document.title = "Modifying the DOM";
+// console.log(document.title)
+
+/* 3) Change the background color of the body to hot pink (#FF69B4).
+*/
+
+// document.body.style = 'background: #FF69B4;'; 
+// console:log(document.body);
+
+/* 4) 
+*/
+
+// Using the children method and a for ... of  loop, display every children elements
+
+// console.log(document.body.children) // this is an array
+// for (a of document.body.children){
+//     console.log(a)
+// }
+
