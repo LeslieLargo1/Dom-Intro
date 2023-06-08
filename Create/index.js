23fae9c2afb42384507af4b5edd94264f8974cbd
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-06-08 15:05:43 +0200

    with loop & style

--- Create/index.js
+++ Create/index.js
@@ -1,16 +1,19 @@
+const names = ['Ignace', 'Leslie','John','safa']
+// in this part we want to link the newly created newSection to the already existing html Element
+// first we have to pinpoint where we want to paste it 
+
+let articleSection = document.querySelector('article');
+
+for (let i=0; i<names.length; i++){
 
 /* create new section */
 let newSection = document.createElement('section');
-
-
 /*create new paragraph */
 let newParagraph1 = document.createElement('p');
-
-
 /* create new text for the paragrap*/
-let newText = document.createTextNode('Ignace');
-
-
+let newText = document.createTextNode(names[i]);
+//wichname=wichname+1
+//wichname++
 
 
 /*  in this part we will link all the created variables to each other */
@@ -21,13 +24,26 @@ newParagraph1.appendChild(newText);
 
 // linked
 newSection.appendChild(newParagraph1);
-
-
-// in this part we want to link the newly created newSection to the already existing html Element
-// first we have to pinpoint where we want to paste it 
-
-let articleSection = document.querySelector('article');
 articleSection.appendChild(newSection);
+
+// generate a random color code
+const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+newParagraph1.style.backgroundColor = randomColor;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*   to see the results on web inspect */
 // console.log('section');
