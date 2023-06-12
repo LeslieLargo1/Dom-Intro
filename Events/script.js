f61219a4211e0a2d3b21e2718de4a04b7a4b0274
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-06-12 16:02:09 +0200

    part 1 & 2

--- Events/script.js
+++ Events/script.js
@@ -5,7 +5,27 @@ const getElapsedTime = () => {
 }
 
 const clickOnSquare = (e) => {
-  console.log(e.target.classList[1])
+  // 1) working area
+  const displayWrapper = document.querySelector('.displayedsquare-wrapper');
+  const unOrderedList = document.querySelector('ul');
+  const timelog = getElapsedTime();
+  // 2) create a new div 
+  const newDiv = document.createElement('div');
+  const list = document.createElement('li');
+  // e = clickOnSquare
+  const colors = e.target.classList[1];
+  
+  newDiv.classList.add('displayedsquare', colors);
+  displayWrapper.appendChild(newDiv);
+
+  // this is a string with the variables included to display 
+  list.textContent = `[${timelog}] created a new ${colors} square`;
+  unOrderedList.appendChild(list);
+
+
+
+ console.log(e.target.classList[1])
+
   console.log(getElapsedTime())
 }
 
@@ -14,3 +34,16 @@ for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
 
+/*   - Create a new `<div>` with a class `.displayedsquare` 
+and the corresponding clicked color in the div above (`.displayedsquare-wrapper`)  */
+// const actionSquare = document.querySelectorAll('.actionsquare')
+// document.querySelector
+// actionSquares.forEach(square => {
+//   square.addEventListener('click', () =>{
+//     const newDiv = document.createElement('div');
+//     newDiv.classList.add('.displayedsquare');
+//     document..appendChild('newDiv');
+
+//   } )
+  
+// });
