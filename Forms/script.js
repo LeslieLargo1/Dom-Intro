837563ec8b8ff1d1c6be5ec11b7824bd460497d8
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-06-14 15:41:14 +0200

    second exercise

--- Forms/script.js
+++ Forms/script.js
@@ -1,4 +1,4 @@
-/*
+/* 1)
   Add a keyup listener
  on the first input field, 
  so that once you type a letter in this field, 
@@ -12,11 +12,39 @@ const spanName = document.getElementById('display-firstname');
 const printName = function (e) {
  
   spanName.textContent += e.key
-  
-   
+    
  }
 
 inputName.addEventListener('keyup', printName )
+
+
+//  2) Add a keyup listener
+// on the second input (the number input),
+// so that if the age is below 18 the content of the section a-hard-truth remains hidden, otherwise show them this hard to swallow fact.
+
+const numBer = document.getElementById('age')
+
+const ageVerification = (e) => {  
+ 
+
+const hardTruth = document.getElementById('a-hard-truth')
+
+  if ( numBer.value < 18) {
+    hardTruth.style.visibility = 'hidden'
+    // //testing the event function
+    // console.log(e);
+  }
+  else { 
+    hardTruth.style.visibility = 'visible'
+
+  }
+
+
+}
+// //testing numBer
+// console.log(numBer);
+ numBer.addEventListener('keyup', ageVerification)
+
 
 
 
